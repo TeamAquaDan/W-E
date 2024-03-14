803f67203fe9920667152112ce6a472461a74a55
김가영 <kkayoung0@gmail.com>
2024-03-14 10:06:58 +0900

    Init: 서비스 서버 패키지 이름 변경

--- Backend/src/main/java/org/whalebank/backend/BackendApplication.java
+++ Backend/src/main/java/org/whalebank/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package org.example.backend.backend;
+package org.whalebank.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Backend/src/test/java/org/whalebank/backend/BackendApplicationTests.java
+++ Backend/src/test/java/org/whalebank/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package org.example.backend.backend;
+package org.whalebank.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
