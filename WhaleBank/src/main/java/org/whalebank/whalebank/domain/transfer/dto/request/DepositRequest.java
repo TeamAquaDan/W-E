3d5dc25129ed8a8dc1a065cb33aaa9f4ff6b6016
박나린 <xw2nana@gmail.com>
2024-03-19 10:12:01 +0900

    Fix: 입금이체 엔티티 수정

--- WhaleBank/src/main/java/org/whalebank/whalebank/domain/transfer/dto/request/DepositRequest.java
+++ WhaleBank/src/main/java/org/whalebank/whalebank/domain/transfer/dto/request/DepositRequest.java
@@ -10,20 +10,12 @@ import java.util.List;
 @NoArgsConstructor
 public class DepositRequest {
 
-    private LocalDateTime trans_dtm;    // 요청일시
-    private int req_cnt;    // 입금요청건수
+  private LocalDateTime trans_dtm;    // 요청일시
 
-    private List<Deposit> depositList;
-
-    @Getter
-    @NoArgsConstructor
-    public class Deposit {
-        private int tran_no;    // 거래순번
-        private String bank_tran_id;    // 은행거래고유번호
-        private String print_content;   // 입금계좌 인자내역
-        private int trans_amt;  // 거래금액
-        private String req_client_bank_code;    // 요청고객계좌 개설기관 표준코드
-        private String req_client_account_num;  // 요청고객 계좌번호
-    }
+  private String bank_tran_id;    // 은행거래고유번호
+  private String print_content;   // 입금계좌 인자내역
+  private int trans_amt;  // 거래금액
+  private String req_client_bank_code;    // 요청고객계좌 개설기관 표준코드
+  private String req_client_account_num;  // 요청고객 계좌번호
 
 }
