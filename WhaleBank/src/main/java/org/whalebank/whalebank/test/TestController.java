d47d576025a00eb58e62cf51bbbb6983aaa6fe7e
jjin-21 <ljj1900@naver.com>
2024-03-20 15:10:27 +0900

    Feat : Deploy Test

---
+++ WhaleBank/src/main/java/org/whalebank/whalebank/test/TestController.java
@@ -0,0 +1,25 @@
+package org.whalebank.backend.test;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
+    private final TestService service;
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @PostMapping("/insert-test")
+    public TestEntity test() {
+        return service.insert("HELLO!");
+    }
+}
