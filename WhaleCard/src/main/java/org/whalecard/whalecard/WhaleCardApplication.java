25e588c016e6f766c4681b15b1a3c4023a352816
박나린 <xw2nana@gmail.com>
2024-03-20 09:21:19 +0900

    Init: 프로젝트 설정

--- WhaleCard/src/main/java/org/whalecard/whalecard/WhaleCardApplication.java
+++ WhaleCard/src/main/java/org/whalecard/whalecard/WhaleCardApplication.java
@@ -1,4 +1,4 @@
-package org.example.whalecard.whalecard;
+package org.whalecard.whalecard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WhaleCardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WhaleCardApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(WhaleCardApplication.class, args);
+  }
 
 }
---
+++ WhaleCard/src/test/java/org/whalecard/whalecard/WhaleCardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.whalecard.whalecard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhaleCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
