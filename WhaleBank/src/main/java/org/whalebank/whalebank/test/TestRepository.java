178b60da048914756ef1b4a62ea8dbe9f4578dcd
jjin-21 <ljj1900@naver.com>
2024-03-20 16:33:58 +0900

    Feat : Deploy Test

--- WhaleBank/src/main/java/org/whalebank/whalebank/test/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package org.whalebank.backend.test;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity, Integer> {
-
-}
