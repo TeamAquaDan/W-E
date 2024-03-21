e0486344fc702d34d20d6daa49655274917e8bf6
신현중 <qsc3117@naver.com>
2024-03-21 12:46:15 +0900

    Feat: 계좌 거래 내역 필터(전체 지출 수입)

--- frontend/lib/screens/bank_history_page/widgets/bank_history_table.dart
+++ frontend/lib/screens/bank_history_page/widgets/bank_history_table.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/models/account_list_data.dart';
 import 'package:frontend/models/dummy_data_account.dart';
 import 'package:frontend/screens/bank_history_page/widgets/bank_history_card.dart';
 import 'package:frontend/screens/bank_history_page/widgets/trans_type_button.dart';
@@ -15,6 +16,27 @@ class BankHistoryTable extends StatefulWidget {
 
 class _BankHistoryTable extends State<BankHistoryTable> {
   String dropdownValue = trans_type_list.first;
+  List<AccountHistoryData> filteredDataList = [];
+  void setDropDownValue(String value) {
+    setState(() {
+      dropdownValue = value;
+      filterDataList();
+    });
+  }
+
+  void filterDataList() {
+    setState(() {
+      if (dropdownValue == '전체') {
+        filteredDataList = dummyDataList;
+      } else if (dropdownValue == '지출') {
+        filteredDataList =
+            dummyDataList.where((data) => data.trans_type == 2).toList();
+      } else if (dropdownValue == '수입') {
+        filteredDataList =
+            dummyDataList.where((data) => data.trans_type == 3).toList();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,12 +47,13 @@ class _BankHistoryTable extends State<BankHistoryTable> {
           DropdownButtonHistory(
             trans_type_list: trans_type_list,
             dropdownValue: dropdownValue,
+            setDropDownValue: setDropDownValue,
           ),
           Spacer(),
           Text(dropdownValue)
         ]),
-        for (int i = 0; i < dummyDataList.length; i++)
-          BankHistoryCard(data: dummyDataList[i])
+        for (int i = 0; i < filteredDataList.length; i++)
+          BankHistoryCard(data: filteredDataList[i])
       ],
     );
   }
