98961f11e759fc07c9649b979393b3e0bcf79d7c
jjin-21 <ljj1900@naver.com>
2024-03-21 11:38:10 +0900

    Feat : Deploy Test

--- Backend/src/main/java/org/whalebank/backend/test/TestController.java
+++ Backend/src/main/java/org/whalebank/backend/test/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/test")
+@RequestMapping("/api")
 @RestController
 @RequiredArgsConstructor
 public class TestController {
