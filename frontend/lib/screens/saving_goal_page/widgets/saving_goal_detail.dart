256c8965fc4f4c17e8c2717ed2201b3d0b9d6be5
신현중 <qsc3117@naver.com>
2024-03-22 16:17:52 +0900

    Feat: 저축 목표 상세 보기

--- frontend/lib/screens/saving_goal_page/widgets/saving_goal_detail.dart
+++ frontend/lib/screens/saving_goal_page/widgets/saving_goal_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/api/save/goal_detail_api.dart';
 import 'package:intl/intl.dart';
 
 class SavingGoalDetail extends StatefulWidget {
@@ -11,32 +12,19 @@ class SavingGoalDetail extends StatefulWidget {
 }
 
 class _SavingGoalDetailState extends State<SavingGoalDetail> {
-  late Map<dynamic, dynamic> goalDetails = {
-    "goal_id": 1,
-    "goal_name": "더미 데이터",
-    "goal_amt": 5000000,
-    "status": 0,
-    "start_date": "2024.01.01",
-    "withdraw_date": "2024.06.01",
-    "end_date": "2024.12.31",
-    "percentage": 40,
-    "saved_amt": 2000000,
-    "category": "여행"
-  }; // 여기에 API 응답 데이터를 저장합니다.
-
+  // late Map<dynamic, dynamic> goalDetails = {}; // 여기에 API 응답 데이터를 저장합니다.
+  late Future<Map<dynamic, dynamic>> _goalDetailsFuture;
   @override
   void initState() {
     super.initState();
-    loadGoalDetail(); // initState에서 데이터 로딩을 시작합니다.
+    _goalDetailsFuture = loadGoalDetail(); // initState에서 데이터 로딩을 시작합니다.
   }
 
-  Future<void> loadGoalDetail() async {
-    // 여기에 REST API 요청을 수행하는 코드를 작성합니다.
-    // 예시로, 다음은 가상의 데이터 로딩 함수입니다.
-    var fetchedGoalDetails = await fetchGoalDetailsFromAPI();
-    setState(() {
-      goalDetails = fetchedGoalDetails; // API로부터 받아온 데이터를 상태에 저장합니다.
-    });
+  Future<Map<dynamic, dynamic>> loadGoalDetail() async {
+    var fetchedGoalDetails = await getGoalDetail(widget.goalId);
+    print('요청 $fetchedGoalDetails');
+    return fetchedGoalDetails ??=
+        fetchedGoalDetails = await fetchGoalDetailsFromAPI();
   }
 
   Future<Map> fetchGoalDetailsFromAPI() async {
@@ -61,52 +49,74 @@ class _SavingGoalDetailState extends State<SavingGoalDetail> {
 
   @override
   Widget build(BuildContext context) {
-    final int toSaveAmount =
-        (goalDetails['goal_amt'] ?? 0) - (goalDetails['saved_amt'] ?? 0);
     return Scaffold(
       appBar: AppBar(
         title: const Text('목표 상세'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 45.0, vertical: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '${goalDetails["goal_name"]} 까지',
-              style: const TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              '${formatNumber(toSaveAmount)}원',
-              style: const TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text('${goalDetails['percentage']}%',
+      body: FutureBuilder<Map<dynamic, dynamic>>(
+        future: _goalDetailsFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: Container(
+                  width: 200,
+                  height: 200,
+                  child: CircularProgressIndicator(strokeWidth: 4)),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+            final goalDetails = snapshot.data!;
+            final int toSaveAmount = (goalDetails['goal_amt'] ?? 0) -
+                (goalDetails['saved_amt'] ?? 0);
+            return Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 45.0, vertical: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${goalDetails["goal_name"]} 까지',
                     style: const TextStyle(
-                      fontSize: 20,
+                      fontSize: 20.0,
                       fontWeight: FontWeight.bold,
-                    )),
-              ],
-            ),
-            const SizedBox(height: 5),
-            LinearProgressIndicator(
-              value: goalDetails["percentage"] / 100, // 70% 진행
-              backgroundColor: const Color.fromARGB(255, 135, 146, 150),
-              valueColor:
-                  const AlwaysStoppedAnimation<Color>(Color(0xFF46A1F5)),
-              minHeight: 5,
-            ),
-          ],
-        ),
+                    ),
+                  ),
+                  Text(
+                    '${formatNumber(toSaveAmount)}원',
+                    style: const TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text('${goalDetails['percentage']}%',
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ],
+                  ),
+                  const SizedBox(height: 5),
+                  LinearProgressIndicator(
+                    value: goalDetails["percentage"] != null
+                        ? goalDetails["percentage"] / 100
+                        : 0.0,
+                    backgroundColor: const Color.fromARGB(255, 135, 146, 150),
+                    valueColor:
+                        const AlwaysStoppedAnimation<Color>(Color(0xFF46A1F5)),
+                    minHeight: 5,
+                  ),
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
