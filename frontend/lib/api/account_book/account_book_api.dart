e3d5a1ad95f8cec696a120c6fe3aee85fdce5a46
shin3117 <qsc3117@naver.com>
2024-03-26 22:04:57 +0900

    Feat: post, patch, delete

--- frontend/lib/api/account_book/account_book_api.dart
+++ frontend/lib/api/account_book/account_book_api.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:frontend/api/account_book/account_book_model.dart';
 import 'package:frontend/services/dio_service.dart';
 import '../base_url.dart';
 
@@ -26,6 +27,57 @@ Future getAccountBookChart(int year, int month) async {
     Response response = await dioService.dio.get(
       '${baseURL}api/accountbook/statistics?year=$year&month=$month',
     );
+
+    // Handle response
+    print('Response status: ${response.statusCode}');
+    print('Response data: ${response.data}');
+    return response.data;
+  } catch (error) {
+    // Handle error
+    print('Error sending POST request: $error');
+    return null;
+  }
+}
+
+Future postAccountBook(AccountBook body) async {
+  final DioService dioService = DioService();
+  try {
+    Response response =
+        await dioService.dio.post('${baseURL}api/accountbook', data: body);
+
+    // Handle response
+    print('Response status: ${response.statusCode}');
+    print('Response data: ${response.data}');
+    return response.data;
+  } catch (error) {
+    // Handle error
+    print('Error sending POST request: $error');
+    return null;
+  }
+}
+
+Future patchAccountBook(AccountBook body, int account_book_id) async {
+  final DioService dioService = DioService();
+  try {
+    Response response = await dioService.dio
+        .patch('${baseURL}api/accountbook/${account_book_id}', data: body);
+
+    // Handle response
+    print('Response status: ${response.statusCode}');
+    print('Response data: ${response.data}');
+    return response.data;
+  } catch (error) {
+    // Handle error
+    print('Error sending POST request: $error');
+    return null;
+  }
+}
+
+Future deleteAccountBook(AccountBook body, int account_book_id) async {
+  final DioService dioService = DioService();
+  try {
+    Response response = await dioService.dio
+        .delete('${baseURL}api/accountbook/${account_book_id}');
 
     // Handle response
     print('Response status: ${response.statusCode}');
