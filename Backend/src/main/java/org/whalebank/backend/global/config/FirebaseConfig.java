96e83013c225550223f984998ec721013de30554
김가영 <kkayoung0@gmail.com>
2024-03-27 11:55:53 +0900

    fix: 파이어베이스 설정 변경

--- Backend/src/main/java/org/whalebank/backend/global/config/FirebaseConfig.java
+++ Backend/src/main/java/org/whalebank/backend/global/config/FirebaseConfig.java
@@ -8,26 +8,33 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.whalebank.backend.global.exception.CustomException;
+import org.whalebank.backend.global.response.ResponseCode;
 
 @Configuration
 public class FirebaseConfig {
 
   @Bean
-  FirebaseMessaging firebaseMessaging() throws IOException {
+  FirebaseMessaging firebaseMessaging() {
 
-    FirebaseApp firebaseApp = null;
+    try {
+      FirebaseApp firebaseApp = null;
 
-    FileInputStream serviceAccount =
-        new FileInputStream(
-            "src/main/resources/key/whale-f56e3-firebase-adminsdk-ows0f-2bf63a2a02.json");
+      FileInputStream serviceAccount =
+          new FileInputStream(
+              "src/main/resources/key/whale-f56e3-firebase-adminsdk-ows0f-2bf63a2a02.json");
 
-    FirebaseOptions options = new FirebaseOptions.Builder()
-        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-        .setDatabaseUrl("https://whale-f56e3-default-rtdb.asia-southeast1.firebasedatabase.app")
-        .build();
+      FirebaseOptions options = new FirebaseOptions.Builder()
+          .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+          .setDatabaseUrl("https://whale-f56e3-default-rtdb.asia-southeast1.firebasedatabase.app")
+          .build();
 
-    firebaseApp = FirebaseApp.initializeApp(options);
-    return FirebaseMessaging.getInstance(firebaseApp);
+      firebaseApp = FirebaseApp.initializeApp(options);
+      return FirebaseMessaging.getInstance(firebaseApp);
+    } catch(IOException e) {
+      e.printStackTrace();
+      throw new CustomException(ResponseCode.FIREBASE_SETTING_FAIL);
+    }
 
   }
 
