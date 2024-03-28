3de272db28d032f91efdda76008b5f2b27ce09f7
jjin-21 <ljj1900@naver.com>
2024-03-28 16:29:09 +0900

    Feat: chat test

--- WhaleChat/main.py
+++ WhaleChat/main.py
@@ -5,8 +5,8 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-# model = AutoModelForCausalLM.from_pretrained('/home/ubuntu/we_model/we_model')
-# tokenizer = AutoTokenizer.from_pretrained('/home/ubuntu/we_model/we_model')
+model = AutoModelForCausalLM.from_pretrained('/home/ubuntu/we_model/we_model')
+tokenizer = AutoTokenizer.from_pretrained('/home/ubuntu/we_model/we_model')
 
 class UserInput(BaseModel):
     user_input: str
@@ -16,32 +16,32 @@ def test():
     return "fastapi 성공"
 
 
-# @app.post("/fastapi/chatbot")
-# def chat(input_data: UserInput):
-#     user_input = input_data.user_input
-#     if not user_input:
-#         raise HTTPException(status_code=400, detail="user_input은 비어 있을 수 없습니다.")
+@app.post("/fastapi/chatbot")
+def chat(input_data: UserInput):
+    user_input = input_data.user_input
+    if not user_input:
+        raise HTTPException(status_code=400, detail="user_input은 비어 있을 수 없습니다.")
 
-#     template = "당신은 w-e 서비스의 주요기능을 설명하고 금융용어를 쉽게 설명해주는 웨일뱅크의 청소년 선생이야. 주어진 질문에 이해하기 쉽게 설명해. "
-#     question = user_input
-#     question = template+question
-#     # 질문을 토크나이저로 인코딩하여 모델에 입력 가능한 형태로 변환
-#     input_ids = tokenizer.encode(question, return_tensors='pt')
+    template = "당신은 w-e 서비스의 주요기능을 설명하고 금융용어를 쉽게 설명해주는 웨일뱅크의 청소년 선생이야. 주어진 질문에 이해하기 쉽게 설명해. "
+    question = user_input
+    question = template+question
+    # 질문을 토크나이저로 인코딩하여 모델에 입력 가능한 형태로 변환
+    input_ids = tokenizer.encode(question, return_tensors='pt')
 
-#     # 답변 생성
-#     output_ids = model.generate(
-#         input_ids,
-#         do_sample=False,
-#         max_length=200,
-#         num_beams=5,
-#         pad_token_id=tokenizer.eos_token_id,
-#         temperature=0.7,
-#         # top_k=top_k,
-#         top_p=0.9,
-#         num_return_sequences=1,
-#         no_repeat_ngram_size=2,
-#     )
+    # 답변 생성
+    output_ids = model.generate(
+        input_ids,
+        do_sample=False,
+        max_length=200,
+        num_beams=5,
+        pad_token_id=tokenizer.eos_token_id,
+        temperature=0.7,
+        # top_k=top_k,
+        top_p=0.9,
+        num_return_sequences=1,
+        no_repeat_ngram_size=2,
+    )
 
-#     # 생성된 답변 디코딩
-#     response = tokenizer.decode(output_ids[0], skip_special_tokens=True)
-#     return {"response": response}
+    # 생성된 답변 디코딩
+    response = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+    return {"response": response}
