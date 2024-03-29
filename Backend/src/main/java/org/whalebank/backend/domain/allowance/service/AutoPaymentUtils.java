70d1de11767ef8f1a14b32d72294703b4551c045
김가영 <kkayoung0@gmail.com>
2024-03-29 17:22:38 +0900

    fix: 도커 파일 시간대 설정

--- Backend/src/main/java/org/whalebank/backend/domain/allowance/service/AutoPaymentUtils.java
+++ Backend/src/main/java/org/whalebank/backend/domain/allowance/service/AutoPaymentUtils.java
@@ -26,7 +26,7 @@ public class AutoPaymentUtils {
   private final AccountService accountService;
   private final FcmUtils fcmUtils;
 
-  @Scheduled(cron = "0 13 17 * * *")
+  @Scheduled(cron = "0 28 17 * * *")
   public void allowanceAutoPayment() {
     // 오늘 날짜에 해당하는 자동이체만 가져와서
     LocalDate today = LocalDate.now();
