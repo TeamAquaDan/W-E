14b2cd05a7834e733548ad1b658097ac516ca1b6
shin3117 <qsc3117@naver.com>
2024-03-30 19:02:22 +0900

    Feat: 가계부 년 월 선택

--- frontend/lib/screens/account_book/account_book_home_page.dart
+++ frontend/lib/screens/account_book/account_book_home_page.dart
@@ -4,6 +4,7 @@ import 'package:frontend/screens/account_book/form_account_book.dart';
 import 'package:frontend/screens/account_book/widgets/chart.dart';
 import 'package:frontend/screens/account_book/widgets/table.dart';
 import 'package:get/get.dart';
+import 'package:month_picker_dialog/month_picker_dialog.dart';
 
 class AccountBookHomePage extends StatefulWidget {
   const AccountBookHomePage({super.key});
@@ -15,16 +16,18 @@ class AccountBookHomePage extends StatefulWidget {
 class _AccountBookHomePageState extends State<AccountBookHomePage> {
   Map<String, dynamic> responseChartData = {};
   Map<String, dynamic> responseData = {};
+  int year = DateTime.now().year;
+  int month = DateTime.now().month;
   @override
   void initState() {
     super.initState();
-    fetchData();
+    fetchData(year, month);
   }
 
-  void fetchData() async {
+  void fetchData(int year, int month) async {
     try {
-      var response = await getAccountBookChart(2024, 3);
-      var response2 = await getAccountBook(2024, 3);
+      var response = await getAccountBookChart(year, month);
+      var response2 = await getAccountBook(year, month);
       setState(() {
         responseChartData = response;
         responseData = response2;
@@ -37,7 +40,7 @@ class _AccountBookHomePageState extends State<AccountBookHomePage> {
   }
 
   void refreshData() {
-    fetchData();
+    fetchData(year, month);
   }
 
   @override
@@ -61,6 +64,29 @@ class _AccountBookHomePageState extends State<AccountBookHomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
+            Row(
+              children: [
+                const Spacer(),
+                Text('$year년 $month월'),
+                IconButton(
+                  onPressed: () {
+                    showMonthPicker(
+                      context: context,
+                      firstDate: DateTime(DateTime.now().year - 1, 1),
+                      lastDate: DateTime(DateTime.now().year + 1, 12),
+                      initialDate: DateTime.now(),
+                    ).then((date) {
+                      if (date != null) {
+                        year = date.year;
+                        month = date.month;
+                        refreshData(); // 데이터 갱신
+                      }
+                    });
+                  },
+                  icon: const Icon(Icons.calendar_today), // 아이콘 변경
+                ),
+              ],
+            ),
             AccountBookChart(data: responseChartData),
             SizedBox(height: 8),
             AccountBookTable(
