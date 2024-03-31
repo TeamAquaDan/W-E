d2c76f7a2f73690aa93f579a5dcdc72f161ce68e
박나린 <xw2nana@gmail.com>
2024-03-31 18:44:31 +0900

    Fix: 더치페이 방 목록 조회 DTO 수정

--- Backend/src/main/java/org/whalebank/backend/domain/dutchpay/dto/response/DutchpayRoomResponseDto.java
+++ Backend/src/main/java/org/whalebank/backend/domain/dutchpay/dto/response/DutchpayRoomResponseDto.java
@@ -12,6 +12,8 @@ import org.whalebank.backend.domain.dutchpay.DutchpayRoomEntity;
 public class DutchpayRoomResponseDto {
 
   private int room_id; // 방아이디
+  private String room_name; // 방 제목
+  private String dutchpay_date; // 결제 일자
   private int manager_id; // 방장
   private boolean is_completed; // 완료 여부
   private int member_num; // 총 인원수
@@ -22,6 +24,8 @@ public class DutchpayRoomResponseDto {
     return DutchpayRoomResponseDto
         .builder()
         .room_id(dutchpayRoom.getRoomId())
+        .room_name(dutchpayRoom.getRoomName())
+        .dutchpay_date(String.valueOf(dutchpayRoom.getDutchpayDate()))
         .manager_id(dutchpayRoom.getManagerId())
         .is_completed(dutchpayRoom.isCompleted())
         .member_num(profileImg.size())  // 방장 포함 인원수
