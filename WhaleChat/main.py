a9ca95efbda8d3b67f6bc5a0947710a432d864e3
jjin-21 <ljj1900@naver.com>
2024-04-01 12:33:23 +0900

    Feat: gpu test

--- WhaleChat/main.py
+++ WhaleChat/main.py
@@ -17,7 +17,7 @@ def test():
 @app.post("/fastapi/chatbot")
 async def chat(input_data: UserInput):
     user_input = input_data.user_input
-    gpu_server_url = "http://70.12.130.131:8001/gpu/chatbot"
+    gpu_server_url = "http://70.12.130.131:8000/gpu/chatbot"
 
     template = "이 시스템은 금융과 관련된 용어를 설명하는 시스템입니다. 금용용어을 어려워하는 어린이들을 위해 금융 용어들을 쉽게 설명해야합니다.그리고 we가 제공하는 서비스에 대해 명확하게 설명해야합니다. "
     
