d0fdb3918184fe84caa5dd99f1642474de4a47cf
shin3117 <qsc3117@naver.com>
2024-04-03 21:07:54 +0900

    Feat: Pin설정 페이지 키보드 수정

--- frontend/lib/screens/pin_setting_page.dart
+++ frontend/lib/screens/pin_setting_page.dart
@@ -19,6 +19,9 @@ class _SetPinPageState extends State<SetPinPage> {
 
   bool _hasPin = false;
   final _secureKeyboardController = SecureKeyboardController();
+  final _pinCodeTextFieldFocusNode = FocusNode();
+  final _confirmPinCodeTextFieldFocusNode = FocusNode();
+  final _oldPinCodeTextFieldFocusNode = FocusNode();
   @override
   void initState() {
     super.initState();
@@ -63,85 +66,130 @@ class _SetPinPageState extends State<SetPinPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(backgroundColor: Color(0xFF568EF8)),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            // height: 100,
-            decoration: BoxDecoration(color: Color(0xFF568EF8)),
-            child: const Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 30),
-                  child: Text(
-                    'PIN 설정',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40,
-                      fontFamily: 'Aggro',
-                      fontWeight: FontWeight.w700,
-                      height: 0,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: <Widget>[
-                if (_hasPin)
-                  TextField(
-                    controller: _oldPinController,
-                    obscureText: true,
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(labelText: '기존 PIN 입력'),
-                  ),
-                TextField(
-                  controller: _pinController,
-                  obscureText: true,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(labelText: '새 PIN 입력'),
-                  maxLength: 6, // 최대 길이를 6으로 설정
-                ),
-                TextField(
-                  controller: _confirmPinController,
-                  obscureText: true,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(labelText: '새 PIN 확인'),
-                  maxLength: 6, // 최대 길이를 6으로 설정
-                ),
-                Container(
-                  width: 300.0,
-                  height: 56.0,
-                  child: ElevatedButton(
-                    onPressed: _setPin,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xFF568EF8), // 버튼의 배경색
-                    ),
-                    child: const Text(
+    return WithSecureKeyboard(
+      controller: _secureKeyboardController,
+      child: Scaffold(
+        appBar: AppBar(backgroundColor: Color(0xFF568EF8)),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              // height: 100,
+              decoration: BoxDecoration(color: Color(0xFF568EF8)),
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 30),
+                    child: Text(
                       'PIN 설정',
+                      textAlign: TextAlign.center,
                       style: TextStyle(
                         color: Colors.white,
-                        fontSize: 30,
+                        fontSize: 40,
                         fontFamily: 'Aggro',
-                        fontWeight: FontWeight.w400,
+                        fontWeight: FontWeight.w700,
                         height: 0,
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  if (_hasPin)
+                    TextField(
+                      controller: _oldPinController,
+                      focusNode: _oldPinCodeTextFieldFocusNode,
+                      enableInteractiveSelection: false,
+                      obscureText: true,
+                      onTap: () {
+                        showPinKeyBoard(
+                            _oldPinCodeTextFieldFocusNode, _oldPinController);
+                      },
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(labelText: '기존 PIN 입력'),
+                    ),
+                  TextField(
+                    controller: _pinController,
+                    focusNode: _pinCodeTextFieldFocusNode,
+                    enableInteractiveSelection: false,
+                    obscureText: true,
+                    onTap: () {
+                      showPinKeyBoard(
+                          _pinCodeTextFieldFocusNode, _pinController);
+                    },
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(labelText: '새 PIN 입력'),
+                    maxLength: 6, // 최대 길이를 6으로 설정
+                  ),
+                  TextField(
+                    controller: _confirmPinController,
+                    focusNode: _confirmPinCodeTextFieldFocusNode,
+                    enableInteractiveSelection: false,
+                    obscureText: true,
+                    onTap: () {
+                      showPinKeyBoard(_confirmPinCodeTextFieldFocusNode,
+                          _confirmPinController);
+                    },
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(labelText: '새 PIN 확인'),
+                    maxLength: 6, // 최대 길이를 6으로 설정
+                  ),
+                  Container(
+                    width: 300.0,
+                    height: 56.0,
+                    child: ElevatedButton(
+                      onPressed: _setPin,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF568EF8), // 버튼의 배경색
+                      ),
+                      child: const Text(
+                        'PIN 설정',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontFamily: 'Aggro',
+                          fontWeight: FontWeight.w400,
+                          height: 0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  void showPinKeyBoard(FieldFocusNode, Controller) {
+    _secureKeyboardController.show(
+      type: SecureKeyboardType.NUMERIC,
+      focusNode: FieldFocusNode,
+      onKeyPressed: (SecureKeyboardKey key) {
+        if (key.action == SecureKeyboardKeyAction.BACKSPACE) {
+          if (Controller.text.isNotEmpty) {
+            Controller.text =
+                Controller.text.substring(0, Controller.text.length - 1);
+            // pin = Controller.text;
+          }
+        } else if (key.action == SecureKeyboardKeyAction.CLEAR) {
+          Controller.text = '';
+          // pin = Controller.text;
+        } else {
+          if (Controller.text.length < 6) {
+            Controller.text += key.text ?? '';
+            // pin = Controller.text;
+          }
+        }
+      },
     );
   }
 }
